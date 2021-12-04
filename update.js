c8cde0dc653707c2f21cdf0f67aaa35a235a133b
asgarrrr <caruellej@hotmail.fr>
2021-12-04 16:02:57 +0100

    Test

--- update.js
+++ update.js
@@ -34,7 +34,7 @@ const { WakaTime } 	= require( "wakatime" )
         `Nice to meet you! I'm just a guy without a tomorrow, who likes to develop little things with his poor knowledge.`,
         `Currently, I'm working on <a href='https://github.com/Asgarrrr/Luna'>\`Luna\`</a>, a Discord bot used by ${ lunaGuild } users with very unique features. I'm also working on \`Yuna Unpack\`, a tool for datamining the contents of Epic Seven's files.`,
         `This week, I've worked on ~${ Math.round( wakaData.data.total_seconds / 60 / 60 ) } hours of coding, and drinked ${ coffeeDrink } coffees ☕.`,
-        `###### This presentation is [updated](https://github.com/Asgarrrr) automatically every minute, most recently on ${ lastUpdate } ( UTC±0 ).`
+        `###### This presentation is [updated](https://github.com/Asgarrrr) automatically every minute, most recently on ${ lastUpdate } ( UTC±0 ).`,
         `\`\`\`\n${ languageProgress }\n\`\`\``,
     ].join( "\n\n" );
 
