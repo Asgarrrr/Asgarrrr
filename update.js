36fb85fd23a9a36995125b19a2fa3dd7b365186d
Jérémy Caruelle <caruellej@hotmail.fr>
2022-05-06 20:42:08 +0200

    Update update.js
    It's better without the point!

--- update.js
+++ update.js
@@ -33,7 +33,7 @@ const { WakaTime } 	= require( "wakatime" )
         `<h1>   <img src="./spoink.gif" style="vertical-align:middle;" width="30px">   Hello ! </h1>`,
         `Nice to meet you!`,
         `Currently, I'm working on <a href='https://github.com/Asgarrrr/Luna'>\`Luna\`</a>, a Discord bot used by ${ lunaGuild } users with very unique features. I'm also working on \`Yuna Unpack\`, a tool for datamining the contents of Epic Seven's files.`,
-        `This week, I've worked on ~${ Math.round( wakaData.data.total_seconds / 60 / 60 ) } hours of coding, and drinked ${ coffeeDrink } coffees ☕.`,
+        `This week, I've worked on ~${ Math.round( wakaData.data.total_seconds / 60 / 60 ) } hours of coding, and drinked ${ coffeeDrink } coffees ☕`,
         `\`\`\`\n${ languageProgress }\n\`\`\``,
         `###### This presentation is [updated](https://github.com/Asgarrrr) automatically every 5 minute, most recently on ${ lastUpdate } ( UTC±0 ).`,
     ].join( "\n\n" );
