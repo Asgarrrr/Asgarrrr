7403e65d52b3066ba33a4a527e78b26e526d3fe5
asgarrrr <caruellej@hotmail.fr>
2022-09-29 10:01:40 +0200

    Never gonna give you up

---
+++ hello.sh
@@ -0,0 +1,12 @@
+
+echo "Rick Astley - Never Gonna Give You Up"
+
+echo "We're no strangers to love"
+echo "You know the rules and so do I"
+echo "A full commitment's what I'm thinking of"
+echo "You wouldn't get this from any other guy"
+echo "I just wanna tell you how I'm feeling"
+echo "Gotta make you understand"
+echo "**Never gonna give you up**"
+
+echo "Never run shell scripts from the internet, kids."
