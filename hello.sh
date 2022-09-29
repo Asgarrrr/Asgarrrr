c5398d5c83f2c2f37ddbe8f052f9a276369909b6
asgarrrr <caruellej@hotmail.fr>
2022-09-29 10:07:19 +0200

    $(tput bold)

--- hello.sh
+++ hello.sh
@@ -1,12 +1,11 @@
+echo "Rick Astley - Never Gonna Give You Up
 
-echo "Rick Astley - Never Gonna Give You Up"
+We're no strangers to love
+You know the rules and so do I
+A full commitment's what I'm thinking of
+You wouldn't get this from any other guy
+I just wanna tell you how I'm feeling
+Gotta make you understand
+$(tput bold)Never gonna give you up$
 
-echo "We're no strangers to love"
-echo "You know the rules and so do I"
-echo "A full commitment's what I'm thinking of"
-echo "You wouldn't get this from any other guy"
-echo "I just wanna tell you how I'm feeling"
-echo "Gotta make you understand"
-echo "**Never gonna give you up**"
-
-echo "Never run shell scripts from the internet, kids."
+\033[0;31mNever run shell scripts from the internet, kids."
