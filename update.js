992afbb4fc746cc2bf19e951f5b782a1cddf1a20
asgarrrr <caruellej@hotmail.fr>
2022-10-07 10:29:52 +0200

    https://github.com/Asgarrrr/Asgarrrr/blob/main/update.js

--- update.js
+++ update.js
@@ -79,7 +79,7 @@ void async function main( ) {
         "└── Databases",
         "    └── MySQL       SQLite      MongoDB         Redis",
         "```",
-        `###### This presentation is [updated](https://github.com/Asgarrrr) automatically every 2 hours, most recently on ${ lastUpdate } ( UTC±2 )`
+        `###### This presentation is [updated](https://github.com/Asgarrrr/Asgarrrr/blob/main/update.js) automatically every 2 hours, most recently on ${ lastUpdate } ( UTC±2 )`
     );
 
     // ── Update README.md
